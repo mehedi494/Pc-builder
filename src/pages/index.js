bba9900389fd89c74aba240a30b57d472758bfab
mehedi hasan <ehasanmunna84@gmail.com>
2023-09-04 19:09:20 +0600

    [DONE] reduce code from home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,15 @@
 import Head from "next/head";
 
 import Layouts from "@/components/Layouts";
-import { Button, Carousel, Radio } from "antd";
-import { useState } from "react";
+import ProductCard from "@/components/Ui/Card";
+import { Button, Carousel } from "antd";
 import Image from "next/image";
+import { useState } from "react";
 import image1 from "../assets/apple-macbook-pro-late-2021-apple-m1-pro-142-11657363629.webp";
 import image2 from "../assets/asus-x415ka-intel-cdc-n4500-14-inch-slate-grey-11667307058.webp";
-// import image3 from "../assets/Macbook-Transparent-Background.png"
-// import image4 from "../assets/pngwing.com.png"
+import image3 from "../assets/dynabook-toshiba-satellite-pro-c40-g-109-intel-11681618797.webp";
+import image4 from "../assets/dynabook-toshiba-satellite-pro-c40-g-11i-intel-11663225340.webp";
+import image5 from "../assets/lenovo-ideapad-d330-10igl-intel-cdc-n4020-101-11646051236.webp";
 
 export default function Home() {
   const contentStyle = {
@@ -17,10 +19,38 @@ export default function Home() {
     display: "flex",
   };
 
-  const [dotPosition, setDotPosition] = useState("right");
+  const [dotPosition, setDotPosition] = useState("bottom");
   const handlePositionChange = ({ target: { value } }) => {
     setDotPosition(value);
   };
+
+  const bannerData = [
+    {
+      img: image1,
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium beatae expedita amet aliquam quis voluptates ",
+    },
+    {
+      img: image2,
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium beatae expedita amet aliquam quis voluptates ",
+    },
+    {
+      img: image3,
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium beatae expedita amet aliquam quis voluptates ",
+    },
+    {
+      img: image4,
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium beatae expedita amet aliquam quis voluptates ",
+    },
+    {
+      img: image5,
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium beatae expedita amet aliquam quis voluptates ",
+    },
+  ];
 
   return (
     <>
@@ -30,79 +60,43 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div style={{ minHeight: "100vh", maxWidth: "100vw" }}>
+      <div style={{ minHeight: "100vh", maxWidth: "100vw" }} className="bg-gray-100">
         <div>
-      
           <Carousel autoplay dotPosition={dotPosition}>
-            <div>
-              <div style={contentStyle}>
-                <div className="w-full flex justify-center items-center  flex-col px-4 ">
-                  <h1 className="font-bold text-3xl text-justify  text-black">
-                    Your dream is come true Macbook pro 2020 edition is here
-                  </h1>
-                  <p className="my-1 text-green-500">
-                    so lets grab your device
-                  </p>
-                  <Button type="dashed">Get started </Button>
-                </div>
-                <div className="w-full flex justify-center items-center   object-contain">
-                  <Image className="w-full" alt="" src={image1}></Image>
-                </div>
-              </div>
-            </div>
-
-            <div>
-              <div style={contentStyle}>
-                <div className="w-full flex justify-center items-center  flex-col px-4 ">
-                  <h1 className="font-bold text-3xl text-justify  text-black">
-                    Your dream is come true Macbook pro 2020 edition is here
-                  </h1>
-                  <p className="my-1 text-green-500">
-                    so lets grab your device
-                  </p>
-                  <Button type="dashed">Get started </Button>
-                </div>
-                <div className="w-full flex justify-center items-center   object-contain">
-                  <Image className="w-full" alt="" src={image2}></Image>
+            {bannerData.map((product, index) => (
+              <div key={index}>
+                <div style={contentStyle}>
+                  <div className="w-full flex justify-center items-center  flex-col px-4 ">
+                    <h1 className="font-bold text-3xl text-justify  text-black">
+                      {product.description}
+                    </h1>
+                    <p className="my-1 text-blue-900 text-xl ">
+                      <i>So lets grab your device ✌️ </i>
+                    </p>
+                    <Button className="bg-green-600 text-white " type="text">
+                      Get started{" "}
+                    </Button>
+                  </div>
+                  <div className="w-full flex justify-center items-center   object-contain">
+                    <Image className="w-full" alt="" src={product.img}></Image>
+                  </div>
                 </div>
               </div>
-            </div>
-            <div>
-              <div style={contentStyle}>
-                <div className="w-full flex justify-center items-center  flex-col px-4 ">
-                  <h1 className="font-bold text-3xl text-justify  text-black">
-                    Your dream is come true Macbook pro 2020 edition is here
-                  </h1>
-                  <p className="my-1 text-green-500">
-                    so lets grab your device
-                  </p>
-                  <Button type="dashed">Get started </Button>
-                </div>
-                <div className="w-full flex justify-center items-center   object-contain">
-                  <Image className="w-full" alt="" src={image2}></Image>
-                </div>
-              </div>
-            </div>
-            <div>
-              <div style={contentStyle}>
-                <div className="w-full flex justify-center items-center  flex-col px-4 ">
-                  <h1 className="font-bold text-3xl text-justify  text-black">
-                    Your dream is come true Macbook pro 2020 edition is here
-                  </h1>
-                  <p className="my-1 text-green-500">
-                    so lets grab your device
-                  </p>
-                  <Button type="dashed">Get started </Button>
-                </div>
-                <div className="w-full flex justify-center items-center   object-contain">
-                  <Image className="w-full" alt="" src={image2}></Image>
-                </div>
-              </div>
-            </div>
+            ))}
           </Carousel>
 
-          <div className='text-center text-3xl font-bold'>
-            this is best sell  product here section
+          <div className=" ">
+            <p className="text-center text-2xl font-semibold  text-gray-600">
+              Most Treanding Sales in this month
+            </p>
+
+            <div className="grid grid-cols-4">
+              {Array(4)
+                .fill("")
+                .map((some, index) => (
+                  <ProductCard key={index}></ProductCard>
+                ))}
+            </div>
           </div>
         </div>
       </div>
