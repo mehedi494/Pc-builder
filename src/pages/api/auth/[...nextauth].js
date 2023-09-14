615572ceab1aef82c2bfcae82f5aafa1e09975c4
mehedi hasan <ehasanmunna84@gmail.com>
2023-09-14 12:26:53 +0600

    [Done]pc-build protected route  build-x5

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -2,7 +2,6 @@ import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
 
-
 export const authOptions = {
   // Configure one or more authentication providers
   providers: [
@@ -17,10 +16,9 @@ export const authOptions = {
     }),
     // ...add more providers here
   ],
-  pages:{
-    singIn:"/login"
-  }
-  
+  pages: {
+    signIn: "/login",
+  },
 };
 
 export default NextAuth(authOptions);
